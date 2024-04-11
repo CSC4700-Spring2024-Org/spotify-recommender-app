cedf773f9e7468efcd2f71e3546368f55dbde9c0
Giana <gabbas@villanova.edu>
2024-04-11 00:48:04 -0400

    Simplified frontend to 'hello world' in App.js, moved access token request to App.js

--- spotify-recommender/src/App.js
+++ spotify-recommender/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { search } from './searcher.js';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const CLIENT_ID = "459618efb1c940028bc3a20817de0916"
+  const CLIENT_SECRET = "1c7760f078134186b654ec52d4ac0bad"
+
+  // requests Access Token
+  
+  const[accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+  var authParameters = {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+  }
+  
+  fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App" onClick={event => search(accessToken)}>
+      hello world
     </div>
   );
 }

1b081aff54c26fffd57cc23201d9f0de30676d8d
Giana <gabbas@villanova.edu>
2024-04-11 00:50:00 -0400

    Prototyped search function with hard-coded search parameters

--- spotify-recommender/src/searcher.js
+++ spotify-recommender/src/searcher.js
@@ -1,23 +1,25 @@
-const CLIENT_ID = "459618efb1c940028bc3a20817de0916"
-const CLIENT_SECRET = "1c7760f078134186b654ec52d4ac0bad"
+function search(accessToken) {
+        
+    // search Spotify API
+    console.log({accessToken})
+    var endpoint = "https://api.spotify.com/v1/search"
+    var query = "hotel californ"
+    var type = "track"
+    var request = endpoint + '?q=' + query + '&type=' + type 
+    console.log("Search for " + query)
+    console.log(request)
 
-function search() {
-
-    
-  // requests Access Token
-    var accessToken;
-
-    var authParameters = {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    var searchParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
     }
-    
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-        .then(result => result.json())
-        .then(data => console.log(data))
+    fetch(request, searchParameters)
+      .then(response => response.json())
+      .then(data => console.log(data))
+
     }
 
   export { search };

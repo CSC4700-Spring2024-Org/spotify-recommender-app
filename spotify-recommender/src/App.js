6e1fac47e1d44f4c6d35f6e0c06281a51c664f5b
Giana <gabbas@villanova.edu>
2024-04-11 20:17:07 -0400

    Fixed infinite token request error

--- spotify-recommender/src/App.js
+++ spotify-recommender/src/App.js
@@ -14,19 +14,21 @@ function App() {
   const[accessToken, setAccessToken] = useState("");
 
   useEffect(() => {
-  var authParameters = {
-      method: 'POST',
-      headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-  }
+    var authParameters = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
   
-  fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      .then(data => setAccessToken(data.access_token))
-      .catch(error => console.log(error))
-    })
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+        .then(result => result.json())
+        .then(data => setAccessToken(data.access_token))
+        .catch(error => console.log(error))
+    }, [])
+
+    // handles searchbar updates
 
     const[results, setResults] = useState("")
 

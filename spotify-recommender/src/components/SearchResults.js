e6c9a2c6af1e855b863268a3c86c32eeb6bf03dc
Giana <gabbas@villanova.edu>
2024-04-16 16:43:40 -0400

    Styling updates for Searchbar and SearchResults

--- spotify-recommender/src/components/SearchResults.js
+++ spotify-recommender/src/components/SearchResults.js
@@ -4,12 +4,11 @@ const SearchResults = (results) => {
     results = results.results
     
   return (
-    <div>
-        {results ? (
-                  results
-                ) : (
-                  ""
-                )}
+    <div style={{width: 243.83, padding: 3, display: 'flex', flexDirection: 'column', boxShadow: '0px 8px 10px gray', alignItems: 'center', justifyContent: 'center', borderBottomLeftRadius: 5, borderBottomRightRadius: 5}}>
+        <div style={{padding: 3}}>{results[0]}</div>
+        <div style={{padding: 3}}>{results[1]}</div>
+        <div style={{padding: 3}}>{results[2]}</div>
+        <div style={{padding: 3}}>{results[3]}</div>
     </div>
   )
 }
--- spotify-recommender/src/components/Searchbar.js
+++ spotify-recommender/src/components/Searchbar.js
@@ -18,11 +18,15 @@ const Searchbar = (accessToken) => {
   return (
     <div>
         <div>
-            <input id="searchInput" placeholder='Enter a song...' style={{borderRadius: 5, margin: 3, lineHeight: 1.75}} onChange={event => handleChange(document.getElementById("searchInput").value)}/>
-            <button style={{borderRadius: 5, lineHeight: 1.75}} onClick={event => search(accessToken, document.getElementById("searchInput").value)}>Search</button>
-        </div>
-        <div>
-            <SearchResults results={results} />
+            <input id="searchInput" placeholder='Enter a song...' style={{borderRadius: 5, margin: 3, lineHeight: 1.75, border: 'none', outline: 'none', boxShadow: '0px 0px 8px gray'}} onChange={event => handleChange(document.getElementById("searchInput").value)}/>
+            <button style={{borderRadius: 5, lineHeight: 1.75, border: 'none', boxShadow: '0px 0px 8px gray'}} onClick={event => search(accessToken, document.getElementById("searchInput").value)}>Search</button>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              {results ? (
+                <SearchResults results={results} />
+                ) : (
+                  ""
+                )}
+            </div>
         </div>
     </div>
   )

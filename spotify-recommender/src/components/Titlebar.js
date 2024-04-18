751d21351502cd7a7a543fce346ed0a184515b6e
Giana <gabbas@villanova.edu>
2024-04-18 18:05:34 -0400

    Added fun scrolling banner

--- spotify-recommender/src/components/Titlebar.js
+++ spotify-recommender/src/components/Titlebar.js
@@ -1,10 +1,17 @@
+import './Titlebar.css'
+
 const Titlebar = () => {
     return (
       <nav className="navbar static-top navbar-expand-lg" style={{backgroundColor: '#1DB954'}}>
-        <div className="container-fluid">
-          <a className="navbar-brand" href="http://localhost:3000/">
+        <div className="container-fluid" style={{padding: 0}}>
+          <a className="navbar-brand" style={{backgroundColor: '#1DB954', zIndex: 999, padding: 10, paddingLeft: 12}} href="http://localhost:3000/">
             Spotify Recommender
           </a>
+          <div className="scroll-container">
+            <div className="scroll-text">Helped over 4 developers find new songs!</div>
+            <div className="scroll-text">Helped over 4 developers find new songs!</div>
+            <div className="scroll-text">Helped over 4 developers find new songs!</div>
+          </div>
         </div>
       </nav>
     );

300ef4dc62b1cf1d51c14d80b1f848b37258c029
Giana <gabbas@villanova.edu>
2024-04-17 22:48:09 -0400

    Render search results dynamically

--- spotify-recommender/src/components/SearchResults.js
+++ spotify-recommender/src/components/SearchResults.js
@@ -5,10 +5,10 @@ const SearchResults = (results) => {
     
   return (
     <div style={{width: 243.83, padding: 3, display: 'flex', flexDirection: 'column', boxShadow: '0px 8px 10px gray', alignItems: 'center', justifyContent: 'center', borderBottomLeftRadius: 5, borderBottomRightRadius: 5}}>
-        <div style={{padding: 3}}>{results[0]}</div>
-        <div style={{padding: 3}}>{results[1]}</div>
-        <div style={{padding: 3}}>{results[2]}</div>
-        <div style={{padding: 3}}>{results[3]}</div>
+        {results.map((result) => (
+          <div style={{padding: 3}}>{result}</div>
+        ))}
+        
     </div>
   )
 }

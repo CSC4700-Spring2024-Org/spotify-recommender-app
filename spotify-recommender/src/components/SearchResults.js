ed8fb6e650a832658cda86975a09c4ea12b19b3d
Giana <gabbas@villanova.edu>
2024-04-17 23:17:05 -0400

    Search results clickable

--- spotify-recommender/src/components/SearchResults.js
+++ spotify-recommender/src/components/SearchResults.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+
+
 const SearchResults = (results) => {
-    results = results.results
-    
+  results = results.results
+  
+  // click on a result
+  const handleClick = (result) => {
+    console.log("Clicked on", result)
+  }
+
+  // render search results
   return (
     <div style={{width: 243.83, padding: 3, display: 'flex', flexDirection: 'column', boxShadow: '0px 8px 10px gray', alignItems: 'center', justifyContent: 'center', borderBottomLeftRadius: 5, borderBottomRightRadius: 5}}>
-        {results.map((result) => (
-          <div style={{padding: 3}}>{result}</div>
+        {results.map((result, i) => (
+          <div style={{padding: 3}} key={i} onClick={event => handleClick(result)}>{result}</div>
         ))}
         
     </div>

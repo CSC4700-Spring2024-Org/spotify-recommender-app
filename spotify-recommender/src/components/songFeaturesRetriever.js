0eef0bb4879eeb49aecb51acf172941c34db27bf
johnDValencia <157629978+johnDValencia@users.noreply.github.com>
2024-04-20 14:58:05 -0400

    Sorry, now it should be moved.

---
+++ spotify-recommender/src/components/songFeaturesRetriever.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; 
+
+const SpotifyAudioFeatures = ({ trackId }) => {
+  const [audioFeatures, setAudioFeatures] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const accessToken = accessToken; 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      
+      try {
+        const response = await axios.get(
+          `https://api.spotify.com/v1/audio-features/${trackId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        setAudioFeatures(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (trackId) {
+      fetchData();
+    }
+  }, [trackId, accessToken]);
+
+  if (isLoading) return <div>Loading audio features...</div>;
+
+  if (error) return <div>Error: {error.message}</div>;
+
+  if (!audioFeatures) return null;
+
+  // Display the retrieved audio features here (e.g., using JSX)
+  return (
+    <div>
+      <h2>Audio Features</h2>
+      <p>Danceability: {audioFeatures.danceability}</p>
+      <p>Energy: {audioFeatures.energy}</p>
+      <p>Acousticness: {audioFeatures.aucousticness}</p>
+      <p>Instrumentallness: {audioFeatures.instrumentalness}</p>
+      <p>Key: {audioFeatures.key}</p>
+      <p>Liveness: {audioFeatures.liveness}</p>
+      <p>Loudness: {audioFeatures.loudness}</p>
+      <p>Mode: {audioFeatures.mode}</p>
+      <p>Speechiness: {audioFeatures.speechiness}</p>
+      <p>Tempo: {audioFeatures.tempo}</p>
+      <p>Time Signature: {audioFeatures.time_signature}</p>
+      <p>Valence(postitivity): {audioFeatures.valence}</p>
+      {/* Display other features as needed */}
+    </div>
+  );
+};
+
+export default SpotifyAudioFeatures;
